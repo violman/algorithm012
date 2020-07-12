6557e296b5c04c8e1c3170627366f68aee4e4fde
wenle <867768013@qq.com>
2020-07-12 20:41:06 +0800

    add plus one quertion

---
+++ Week_01/plusOne.java
@@ -0,0 +1,24 @@
+public class plusOne {
+  /*
+   * @lc app=leetcode.cn id=66 lang=java
+   *
+   * [66] 加一
+   */
+
+  // @lc code=start
+  //✋
+  public int[] plusOne(int[] digits) {
+    for (int i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] < 9) {
+        digits[i]++;
+        return digits;
+      }
+      digits[i] = 0;
+    }
+    digits = new int[digits.length + 1];
+    digits[0] = 1;
+    return digits;
+  }
+  // @lc code=end
+
+}
